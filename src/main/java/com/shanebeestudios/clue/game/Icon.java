b4b6b5768d009a91cd200b7534de19c84baba062
ShaneBee <shanebolenback@me.com>
2020-04-14 01:54:00 -0700

    Icon - create icon class

---
+++ src/main/java/com/shanebeestudios/clue/game/Icon.java
@@ -0,0 +1,35 @@
+package com.shanebeestudios.clue.game;
+
+import com.shanebeestudios.clue.ClueGame;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Icon {
+
+    public static ImageIcon CLUE_LOGO = getScaledIcon("images/clue-logo.png", "main logo", 128, 54);
+
+    private static ImageIcon createImageIcon(String path, String description) {
+        java.net.URL imgURL = ClueGame.class.getClassLoader().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL, description);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
+    private static ImageIcon getScaledIcon(String path, String description, int w, int h) {
+        java.net.URL imgURL = ClueGame.class.getClassLoader().getResource(path);
+        if (imgURL != null) {
+            ImageIcon i = new ImageIcon(imgURL, description);
+            Image image = i.getImage();
+            Image newImage = image.getScaledInstance(w, h, Image.SCALE_SMOOTH);
+            return new ImageIcon(newImage);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
+}
