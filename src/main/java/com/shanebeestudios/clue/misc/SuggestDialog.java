13bbaa7ac984d8bdd535cf86e5a1d04417d4110e
ShaneBee <shanebolenback@me.com>
2020-04-14 02:20:57 -0700

    SuggestDialog - changes - Add icon to message popups - Use enums instead of strings

--- src/main/java/com/shanebeestudios/clue/misc/SuggestDialog.java
+++ src/main/java/com/shanebeestudios/clue/misc/SuggestDialog.java
@@ -1,37 +1,36 @@
 package com.shanebeestudios.clue.misc;
 
 import com.shanebeestudios.clue.ClueGame;
+import com.shanebeestudios.clue.game.Characters;
+import com.shanebeestudios.clue.game.Icon;
+import com.shanebeestudios.clue.game.Rooms;
+import com.shanebeestudios.clue.game.Weapons;
 
-import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 
 public class SuggestDialog extends JDialog {
 
     public enum SuggestType {
-        SUGGESTION, ACCUSATION;
+        SUGGESTION, ACCUSATION
     }
 
     JButton submit, cancel;
     ClueGame game;
-    JComboBox<String> room;
-    JComboBox<String> person;
-    JComboBox<String> weapon;
+    JComboBox<String> rooms;
+    JComboBox<String> characters;
+    JComboBox<String> weapons;
     SuggestType t;
 
     public SuggestDialog(SuggestType t, ClueGame g) {
         super(g, "", true);
-        room = new JComboBox<>();
-        person = new JComboBox<>();
-        weapon = new JComboBox<>();
+        rooms = new JComboBox<>();
+        characters = new JComboBox<>();
+        weapons = new JComboBox<>();
         game = g;
         this.t = t;
         createLayout(t, g);
@@ -41,42 +40,29 @@ public class SuggestDialog extends JDialog {
 
     public void createLayout(SuggestType t, ClueGame g) {
         setLayout(new GridLayout(4, 2));
-        add(new JLabel("Room"));
+        add(new JLabel("  Room"));
         if (t == SuggestType.ACCUSATION) {
             setTitle("Make an Accusation");
-            room.addItem("Conservatory");
-            room.addItem("Kitchen");
-            room.addItem("Ballroom");
-            room.addItem("Billiard Room");
-            room.addItem("Library");
-            room.addItem("Study");
-            room.addItem("Dining Room");
-            room.addItem("Lounge");
-            room.addItem("Walkway");
-            room.addItem("Closet");
-            room.addItem("Hall");
-            add(room);
+            for (Rooms room : Rooms.values()) {
+                if (room != Rooms.CLOSET && room != Rooms.WALKWAY && room != Rooms.OUTSIDE) {
+                    rooms.addItem(room.getName());
+                }
+            }
         } else {
             setTitle("Make a Guess");
-            room.addItem(g.getBoard().getRooms().get(g.getBoard().getRoomCellAt(g.getHumanPlayer().getRow(), g.getHumanPlayer().getColumn()).getRoomClassifier()));
+            rooms.addItem(g.getBoard().getRooms().get(g.getBoard().getRoomCellAt(g.getHumanPlayer().getRow(), g.getHumanPlayer().getColumn()).getRoomClassifier()));
         }
-        add(room);
-        add(new JLabel("Person"));
-        person.addItem("Miss Scarlet");
-        person.addItem("Mr. Green");
-        person.addItem("Mrs. White");
-        person.addItem("Colonel Mustard");
-        person.addItem("Professor Plum");
-        person.addItem("Mrs. Peacock");
-        add(person);
-        add(new JLabel("Weapon"));
-        weapon.addItem("Revolver");
-        weapon.addItem("Knife");
-        weapon.addItem("Rope");
-        weapon.addItem("Lead Pipe");
-        weapon.addItem("Candlestick");
-        weapon.addItem("Wrench");
-        add(weapon);
+        add(rooms);
+        add(new JLabel("  Person"));
+        for (Characters character : Characters.values()) {
+            characters.addItem(character.getName());
+        }
+        add(characters);
+        add(new JLabel("  Weapon"));
+        for (Weapons weapon : Weapons.values()) {
+            weapons.addItem(weapon.getName());
+        }
+        add(weapons);
         add(submit());
         add(cancel());
     }
@@ -95,13 +81,14 @@ public class SuggestDialog extends JDialog {
 
     class ButtonListener implements ActionListener {
 
+        @SuppressWarnings("ConstantConditions")
         @Override
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() == submit) {
-                Card disprove = game.handleSuggestion(person.getSelectedItem().toString(), room.getSelectedItem().toString(), weapon.getSelectedItem().toString(), game.getHumanPlayer());
-                Solution s = new Solution(person.getSelectedItem().toString(), weapon.getSelectedItem().toString(), room.getSelectedItem().toString());
+                Card disprove = game.handleSuggestion(characters.getSelectedItem().toString(), rooms.getSelectedItem().toString(), weapons.getSelectedItem().toString(), game.getHumanPlayer());
+                Solution s = new Solution(characters.getSelectedItem().toString(), weapons.getSelectedItem().toString(), rooms.getSelectedItem().toString());
                 if (t == SuggestType.SUGGESTION) {
-                    game.getControlPanel().getGuesstext().setText(person.getSelectedItem().toString() + " " + room.getSelectedItem().toString() + " " + weapon.getSelectedItem().toString());
+                    game.getControlPanel().getGuesstext().setText(characters.getSelectedItem().toString() + " " + rooms.getSelectedItem().toString() + " " + weapons.getSelectedItem().toString());
                     if (disprove != null) {
                         game.getControlPanel().getResponse().setText(disprove.getName());
                     } else {
@@ -109,10 +96,11 @@ public class SuggestDialog extends JDialog {
                     }
                 }
                 if (game.checkAccusation(s) && t == SuggestType.ACCUSATION) {
-                    JOptionPane.showMessageDialog(game, "You are correct! Game will now end.", "You Win", JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(game, "You are correct!\nGame will now end.", "You Win", JOptionPane.INFORMATION_MESSAGE, Icon.CLUE_LOGO);
                     System.exit(0);
                 } else if (!game.checkAccusation(s) && t == SuggestType.ACCUSATION) {
-                    JOptionPane.showMessageDialog(game, "That is not correct.", "Incorrect Guess", JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(game, "That is not correct.\nGame will now end.", "Incorrect Guess", JOptionPane.INFORMATION_MESSAGE, Icon.CLUE_LOGO);
+                    System.exit(0);
                 }
             }
             setVisible(false);
